35b7c52339496d1a395a9c2960bc0904257741eb
Dontae Moore <dontae_moore132@mymail.eku.edu>
2021-11-10 23:47:17 -0500

    Revert "Added Main Menu"
    This reverts commit 4ea72c427b5cbcdffacbd627c331710c265df2cb.

--- CSC440GroupProject/Form1.cs
+++ CSC440GroupProject/Form1.cs
@@ -13,30 +13,5 @@ namespace CSC440GroupProject {
         public Form1() {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
